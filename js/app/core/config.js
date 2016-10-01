4292e5408715ab9d3725dabb6f867c9dbc16547e
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-09-30 23:16:47 -0500

    Primer parte de la implementación para poder compartir los links en las redes sociales y que aparezca el preview correspondiente. http://www.michaelbromley.co.uk/blog/171/enable-rich-social-sharing-in-your-angularjs-app

--- js/app/core/config.js
+++ js/app/core/config.js
@@ -10,7 +10,7 @@
 
     core.config(configure);
 
-    function configure ($routeProvider, routehelperConfigProvider) {
+    function configure ($routeProvider, routehelperConfigProvider, $locationProvider) {
 
         // Configure the common route provider
         routehelperConfigProvider.config.$routeProvider = $routeProvider;
@@ -25,5 +25,8 @@
             // }]
         };*/
         //routehelperConfigProvider.config.resolveAlways = resolveAlways;
+
+        // enable HTML5 mode as hashbang-type URLs will not work with mod_rewrite redirection
+        $locationProvider.html5Mode(true).hashPrefix('!');
     }
 })();
