7728ace84f680ce8fd14e4435edc2688136cf1c2
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-10-15 23:48:22 -0500

    Mejora menor en un coppy para el texto de Cargando ...

--- js/app/widgets/loadingIndicator/loadingIndicator.js
+++ js/app/widgets/loadingIndicator/loadingIndicator.js
@@ -9,7 +9,7 @@
 		/*Agregue el template en el código para el tiempo en que ese carge este template en particular sea el minimo*/
 		var directive = {
 			restrict: 'EA',
-		    template: "<div class='container' ng-if='isRouteLoading'><div class='row'><div class='col-md-12 text-center'><h1 class='loading-indicator'>Cargando <i class='fa fa-cog fa-spin'></i></h1></div></div></div>",
+		    template: "<div class='container' ng-if='isRouteLoading'><div class='row'><div class='col-md-12 text-center'><h1 class='loading-indicator'>Cargando ... <i class='fa fa-cog fa-spin'></i></h1></div></div></div>",
 		    link: function(scope, elem, attrs) {
 		        scope.isRouteLoading = false;
 
