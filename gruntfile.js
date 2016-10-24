85de7504b091c436dd5d3ccdfbb5ba6a345d47c3
Diego Mendoza <dmendoza@inflexionsoftware.com>
2016-10-23 23:20:43 -0500

    Primeros pasos con grunt en el proyecto

--- gruntfile.js
+++ gruntfile.js
@@ -9,16 +9,47 @@ module.exports = function(grunt) {
             },
             app: { //"app" target
                 files: {
-                    'js/build-grunt/app.module.js': ['js/app/app.module.js']
+                    'js/build-grunt/min-safe/app.module.js': ['js/app/app.module.js'],
+                    'js/build-grunt/min-safe/blocks/router/router.module.js': ['js/app/blocks/router/router.module.js'],
+                    'js/build-grunt/min-safe/blocks/router/routehelper.js' : ['js/app/blocks/router/routehelper.js']
                 }
             }
+        },
+        concat: {
+            js: { //target
+                src: ['js/build-grunt/min-safe/app.module.js'],
+                dest: 'js/build-grunt/min/app.js'
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            js: { //target
+                src: ['js/app/app.module.js'],
+                dest: 'js/build-grunt/min/app.js'
+            }
         } 
+        /*uglify: {
+            js: {
+              files: [{
+                  expand: true,
+                  cwd: 'js/app',
+                  src: 'js/build-grunt/min/app.js',
+                  dest: 'js/build-grunt/min/app.js'
+              }]
+            }
+          }*/
+
     });
     
     //load grunt tasks
     grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //register grunt default task
-    grunt.registerTask('default', ['ngAnnotate']);
+    //grunt.registerTask('default', ['ngAnnotate','concat','uglify']);
+    grunt.registerTask('default', ['uglify']);
 }
 
